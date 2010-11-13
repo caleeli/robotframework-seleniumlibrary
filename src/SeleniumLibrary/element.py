b5a5becb1defcbaa268aca17480a5db112c8dbdb
Robert Spielmann <spielmann.robert@gmail.com>
2010-11-13 22:32:40 +0100

    Removed mouse move keyword because behaviour is unclear.
    Update issue 118
    Status: Review
    I've removed the mouse move keyword because of unclear behaviour of Selenium. The new keywords are:
    
    * Simulate Mouse Over
    * Simulate Mouse Out
    * Simulate Mouse Down
    * Simulate Mouse Up
    
    Acceptance tests have been implemented and are green. =)

---
+++ src/SeleniumLibrary/element.py
@@ -0,0 +1,66 @@
+#  Copyright 2008-2010 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+class Element(object):
+
+    def get_element_attribute(self, attribute_locator):
+        """Return value of element attribute.
+
+        `attribute_locator` consists of element locator followed by an @ sign
+        and attribute name, for example "element_id@class".
+        """
+        return self._selenium.get_attribute(attribute_locator)
+
+    def get_matching_xpath_count(self, xpath):
+        """Returns number of elements matching `xpath`
+        
+        If you wish to assert that the number of matching elements
+        has a certain value, use `Xpath Should Match X Times`.
+        """
+        return self._selenium.get_xpath_count(xpath)
+
+    def get_horizontal_position(self, locator):
+        """Returns horizontal position of element identified by `locator`.
+
+        The position is returned in pixels off the left side of the page, 
+        as an integer. Fails if a matching element is not found.
+
+        See also `Get Vertical Position`.
+        """
+        return self._get_position(self._selenium.get_element_position_left,
+                                  locator)
+
+    def get_vertical_position(self, locator):
+        """Returns vertical position of element identified by `locator`.
+
+        The position is returned in pixels off the top of the page,
+        as an integer. Fails if a matching element is not found.
+
+        See also `Get Horizontal Position`.
+        """
+        return self._get_position(self._selenium.get_element_position_top,
+                                  locator)
+
+    def _get_position(self, getter, locator):
+        not_found = "Could not determine position for '%s'" % locator
+        try:
+            pos = getter(self._parse_locator(locator))
+        except Exception, err:
+            if not self._error_contains(err, 'not found'):
+                raise
+            raise RuntimeError(not_found)
+        if not pos:
+            raise RuntimeError(not_found)
+        return int(pos)
+
---
+++ test/run_tests.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+
+#  Copyright 2008-2010 Nokia Siemens Networks Oyj
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+import os
+import sys
+from subprocess import Popen, call
+from tempfile import TemporaryFile
+
+from run_unit_tests import run_unit_tests
+
+ROOT = os.path.dirname(__file__)
+TESTDATADIR = os.path.join(ROOT, 'acceptance')
+RESOURCEDIR = os.path.join(ROOT, 'resources')
+SRCDIR = os.path.join(ROOT, '..', 'src')
+UTESTDIR = os.path.join(ROOT, 'unit')
+RESULTDIR = os.path.join(ROOT, 'results')
+HTPPSERVER = os.path.join(RESOURCEDIR, 'testserver', 'testserver.py')
+ROBOT_ARGS = [
+'--doc', 'SeleniumSPacceptanceSPtestsSPwithSP%(browser)s',
+'--outputdir', '%(outdir)s',
+'--variable', 'browser:%(browser)s',
+'--escape', 'space:SP',
+'--report', 'none',
+'--log', 'none',
+#'--suite', '...',
+'--loglevel', 'DEBUG',
+'--pythonpath', '%(pythonpath)s',
+]
+REBOT_ARGS = [
+'--outputdir', '%(outdir)s',
+'--name', '%(browser)sSPAcceptanceSPTests',
+'--escape', 'space:SP',
+'--critical', 'regression',
+'--noncritical', 'inprogress',
+]
+ARG_VALUES = {'outdir': RESULTDIR, 'pythonpath': SRCDIR}
+
+
+def acceptance_tests(interpreter, browser, args):
+    ARG_VALUES['browser'] = browser.replace('*', '')
+    start_http_server()
+    suffix = os.sep == '\\' and 'ybot.bat' or 'ybot'
+    runner = "%s%s" % ('jython' == interpreter and 'j' or 'p', suffix)
+    execute_tests(runner)
+    stop_http_server()
+    return process_output()
+
+def start_http_server():
+    server_output = TemporaryFile()
+    Popen(['python', HTPPSERVER ,'start'],
+          stdout=server_output, stderr=server_output)
+
+def execute_tests(runner):
+    command = [runner] + [ arg % ARG_VALUES for arg in ROBOT_ARGS] + args +\
+            [ TESTDATADIR ]
+    call(command, env=os.environ)
+
+def stop_http_server():
+    call(['python', HTPPSERVER, 'stop'])
+
+def process_output():
+    print
+    call(['python', os.path.join(RESOURCEDIR, 'statuschecker.py'),
+         os.path.join(RESULTDIR, 'output.xml')])
+    rebot = os.sep == '\\' and 'rebot.bat' or 'rebot'
+    rebot_cmd = [rebot] + [ arg % ARG_VALUES for arg in REBOT_ARGS ] + \
+                [os.path.join(ARG_VALUES['outdir'], 'output.xml') ]
+    rc = call(rebot_cmd, env=os.environ)
+    if rc == 0:
+        print 'All critical tests passed'
+    else:
+        print '%d critical test%s failed' % (rc, 's' if rc != 1 else '')
+    return rc
+
+
+if __name__ ==  '__main__':
+    if not len(sys.argv) > 2:
+        print 'Usage:  python run_tests.py python|jython browser [options]'
+        print
+        print 'See README.txt for details.'
+        sys.exit(1)
+    interpreter = ('jython' in sys.argv[1]) and 'jython' or 'python'
+    browser = sys.argv[2].lower()
+    args = sys.argv[3:]
+    if not args:
+        if interpreter == 'jython':
+            print 'This script does not run unit tests with Jython'
+            print 'They can be excecuted with jython test/run_unit_tests.py'
+        else:
+            print 'Running unit tests'
+            failures = run_unit_tests()
+            if failures != 0:
+                print '\n%d unit tests failed - not running acceptance tests!' % failures
+                sys.exit(1)
+            print 'All unit tests passed'
+    if browser != 'unit':
+        sys.exit(acceptance_tests(interpreter, browser, args))
